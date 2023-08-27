2b31830f9a7774d5eb290295e728cc60a6b53dd0
unknown <abhinab.c@fyle.in>
2023-08-27 16:26:49 +0530

    Basic Types added

---
+++ src/commons/types.ts
@@ -0,0 +1,41 @@
+export type UserData = {
+    id: string;
+    name: string;
+    profilePic: string;
+    jobRole: string;
+    pronouns: string;
+    bio: string;
+    
+  };
+export type exp = {
+    id: string;
+    name: string;
+    instagramID: string;
+    twitterID: string;
+    linkedInID: string;
+    profilePic: string;
+    jobRole: string;
+    pronouns: string;
+    bio: string;
+    
+  };
+  
+export type achievements = {
+    id: string;
+    name: string;
+    instagramID: string;
+    twitterID: string;
+    linkedInID: string;
+    profilePic: string;
+    jobRole: string;
+    pronouns: string;
+    bio: string;
+    
+  };
+export type contact = {
+    instagramID: string;
+    twitterID: string;
+    linkedInID: string;
+    githubID: string;
+    email: string;    
+  };
