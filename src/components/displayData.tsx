023906032424bf0a7cc625328d3fd33dbe9fd20e
unknown <abhinab.c@fyle.in>
2023-08-29 19:04:47 +0530

    firts

--- src/components/displayData.tsx
+++ src/components/displayData.tsx
@@ -1,8 +1,22 @@
 import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import { UserData } from "@/commons/types";
 
 type UserProfileProps = {
     userData: UserData;
     
     userStats: any;
+  };
+  const Dummy: React.FC<UserProfileProps> = ({}) => {
+    const router = useRouter();
+    const { username } = router.query;
+    return (
+      <div>
+        <h1>
+  
+        {username}
+        </h1>
+      </div>
+    )
+      
   };
--- src/pages/[username].tsx
+++ src/pages/[username].tsx
@@ -1,10 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { useRouter } from "next/router";
 import axios from "axios";
+import { useRouter } from "next/router";
 import { UserData } from "@/commons/types";
 
-type DisplayDataProps = {
+type UserProfileProps = {
     userData: UserData;
     
     userStats: any;
   };
+
+  
+const UserProfile: React.FC<UserProfileProps> = ({}) => {
+  const router = useRouter();
+  const { username } = router.query;
+  return (
+    <div>
+      <h1>
+
+      {username}
+      </h1>
+    </div>
+  )
+    
+};
